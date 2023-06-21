acc10a3a588d0b0f5cc44266ca30442702332f3c
Chris Ward <cw@yellowdave.co.uk>
2023-06-21 15:35:51 +0100

    Basically working

--- sketch.js
+++ sketch.js
@@ -1,20 +1,59 @@
+let hue1, hue2;
+let saturation1, saturation2;
+let brightness1, brightness2;
+let alpha1, alpha2;
+let ringColor1, ringColor2;
+let easing = 0.05; // 1.0 to 0
+let mix = 0;
+let mixTarget = 0;
+let dynamicColor;
+// Ring stuff
+let numRings;
+let ringWidth;
+let largestRadius;
+let overlap;
+let centerX;
+let centerY;
+
 function setup() {
   createCanvas(560, 560); // Adjust the canvas size as per your needs
+  initRings();
+  initColors();
 }
 
-function draw() {
-  noLoop();
+function initRings() {
+  // Ring stuff
+  numRings = 3; // Number of concentric rings
+  ringWidth = 50; // Width of each ring
+  largestRadius = 250; // Radius of the largest ring
+  overlap = 10; // Amount of overlap between rings
+  centerX = width / 2; // X-coordinate of the center
+  centerY = height / 2; // Y-coordinate of the center
+}
 
-  let backgroundColour = 236;
+function initColors() {
+  // Color stuff
 
-  background(backgroundColour); // Set the background color
+  ringColor1 = newRandomColor();
+  ringColor2 = newRandomColor();
 
-  let numRings = 3; // Number of concentric rings
-  let ringWidth = 50; // Width of each ring
-  let largestRadius = 250; // Radius of the largest ring
-  let overlap = 10; // Amount of overlap between rings
-  let centerX = width / 2; // X-coordinate of the center
-  let centerY = height / 2; // Y-coordinate of the center
+  // ringColor1 = color("#8F28A1"); // warm purple color
+  //ringColor2 = color("#3BEDB7"); // mint green color
+}
+
+function newRandomColor() {
+  let hue = random(360); // Generate a random hue value
+  let saturation = random(50, 255); // Generate a random saturation value within a pastel range
+  let brightness = random(70, 90); // Generate a random brightness value within a pastel range
+  let alpha = 123;
+  let ringColor = color(hue, saturation, brightness, alpha); // Create a color object with the generated values
+
+  return ringColor;
+}
+
+function drawRings() {
+  let backgroundColor = 236;
+  background(backgroundColor); // Set the background color
 
   // Draw the concentric rings
   for (let i = 0; i < numRings; i++) {
@@ -24,23 +63,43 @@ function draw() {
     let innerRadius = outerRadius - ringWidth; // Calculate radius for each ring
     let innerDiameter = innerRadius * 2 - overlap; // Calculate diameter
 
-    let hue = random(360); // Generate a random hue value
-    let saturation = random(50, 70); // Generate a random saturation value within a pastel range
-    let brightness = random(70, 90); // Generate a random brightness value within a pastel range
-    let alpha = 123;
-
-    let ringColor = color(hue, saturation, brightness, alpha); // Create a color object with the generated values
-
     noStroke(); // Disable stroke for rings
-    fill(ringColor); // Set fill color to the generated pastel color
+
+    // let mixTarget = map(mouseX, 0, width, 0.0, 1.0);
+    // mix = mix + (mixTarget - mix) * easing;
+
+    dynamicColor = lerpColor(ringColor1, ringColor2, mix);
+    // console.log(dynamicColor.levels);
+    console.log(mix);
+    fill(dynamicColor); // Set fill color to the generated pastel color
 
     // Draw each ring
     ellipse(centerX, centerY, outerDiameter, outerDiameter);
-    fill(backgroundColour); // Set fill color to the generated pastel color
+    fill(backgroundColor); // Set fill color to the generated pastel color
 
     if (i !== numRings - 1) {
       // Draw each ring
       ellipse(centerX, centerY, innerDiameter, innerDiameter);
     }
   }
+}
+
+function rotateColors() {
+  ringColor1 = ringColor2;
+  ringColor2 = newRandomColor();
+}
+
+function draw() {
+  // frameRate(2);
+
+  mix += 0.005;
+
+  if (mix > 1.0) {
+    rotateColors();
+    mix = 0;
+  }
+
+  drawRings();
+
+  // noLoop();
 }
