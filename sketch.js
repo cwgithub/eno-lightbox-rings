915d4382b45f2e0bbd562531ebd826d8f70402ad
Chris Ward <cw@yellowdave.co.uk>
2023-06-22 20:39:23 +0100

    fix the no. rings

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function mousePressed() {
 
 function initRings() {
   // Ring stuff
-  numRings = random(3, 7); // Number of concentric rings
+  numRings = 3; // Number of concentric rings
   largestRadius = min(height, width) / 2 - ringYOffset; // Radius of the largest ring
   overlap = 0; // Amount of overlap between rings
   centerX = width / 2; // X-coordinate of the center
