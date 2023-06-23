2af969eb850a3706e7815f48b079236e7aa70639
Chris Ward <cw@yellowdave.co.uk>
2023-06-23 13:17:50 +0100

    Mobile full screen work

--- sketch.js
+++ sketch.js
@@ -218,3 +218,15 @@ function playSound() {
   //   droneSounds[dIdx].play();
   // }
 }
+
+/* full screening will change the size of the canvas */
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+/* prevents the mobile browser from processing some default
+ * touch events, like swiping left for "back" or scrolling the page.
+ */
+document.ontouchmove = function (event) {
+  event.preventDefault();
+};
